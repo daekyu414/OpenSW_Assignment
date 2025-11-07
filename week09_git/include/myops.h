09c2f8319a21ae385712d1d78762cf97298d4b86
Daekyu Yang <eorbwogml@inu.ac.kr>
2025-11-07 14:20:14 +0900

    add my basic ops

---
+++ week09_git/include/myops.h
@@ -0,0 +1,11 @@
+#ifndef MYOPS_H
+#define MTOPS_H
+
+#include <stdio.h>
+
+double myadd(double a, double b);
+double mysub(double a, double b);
+double mydiv(double a, double b);
+double mymul(double a, double b);
+
+#endif
